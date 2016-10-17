1af682bfec63824cf62d738071c14d38aa6923e7
glennfield20 <ladesmaglenngarfield@gmail.com>
2016-10-17 16:56:53 +0800

    Override Devise registrations path and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+  # Override Devise Controlers
+  # rails generate devise:controllers
+
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
 
+  # Override Devise path
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
   end
 
   root to: "home#index"
